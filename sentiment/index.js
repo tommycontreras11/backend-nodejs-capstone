4850474b462aab254ed77a106811e6dff19a202e
Tommy11 <tommycontreras34@gmail.com>
2024-07-11 16:31:18 -0400

    fix: modify index from sentiment

--- sentiment/index.js
+++ sentiment/index.js
@@ -37,14 +37,14 @@ app.post('/sentiment', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        if(analysisResult < 0 && analysisResult < 0.33) sentiment = "negative";
-        if(analysisResult > 0) sentiment = "positive";
+        if(analysisResult < 0) sentiment = "negative";
+        if(analysisResult > 0.33) sentiment = "positive";
 
         // Logging the result
         logger.info(`Sentiment analysis result: ${analysisResult}`);
 
         // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
+        return res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
