63c0f1e2610518927ad6c5292b394cf034deb70d
TommyGC11 <tommycontreras34@gmail.com>
2024-07-12 13:29:49 -0400

    Connect backend with frontend app

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
-const authRoutes = require('./routes/authRoutes');
-const searchRoutes = require('./routes/searchRoutes');
+const path = require('path');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -27,39 +25,40 @@ app.use(express.json());
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 //{{insert code here}}
+const authRoutes = require('./routes/authRoutes');
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 //{{insert code here}}
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
-
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/auth', authRoutes);
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
-
+app.use('/api/secondchance/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
-app.use('/api/secondchance/items', secondChanceItemsRoutes);
-app.use('/api/secondchance/search', searchRoutes);
-app.use('/api/auth', authRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Inside the server")
