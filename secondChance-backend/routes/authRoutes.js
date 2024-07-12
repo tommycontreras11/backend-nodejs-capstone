5505b8fe3929a5f0c76087fabd5bb613f9338de6
TommyGC11 <tommycontreras34@gmail.com>
2024-07-12 09:26:38 -0400

    Modify let JWT_SECRET to const

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const logger = require('../logger');
 
-let JWT_SECRET = `${process.env.JWT_SECRET}`;
+const JWT_SECRET = `${process.env.JWT_SECRET}`;
 
 // Add a new user
 router.post('/register', async (req, res) => {
