ea15f09d3a44e5d50e8b0e5f51bd9975e21cbc0b
TommyGC11 <tommycontreras34@gmail.com>
2024-07-12 09:36:27 -0400

    Implemented API endpoints

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -58,4 +58,54 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// Sign in
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection('users')
+
+        // Task 3: Check for user credentials in database
+        let { email, password } = req.body;
+
+        const findUser = await collection.findOne({ email });
+
+        if(!findUser) {
+            logger.error('User not found');
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        const comparePasswords = await bcrypt.compare(password, findUser.password);
+
+        if(!comparePasswords) {
+            logger.error('Passwords do not match');
+            return res.status(404).json({ message: "Passwords do not match" });
+        }
+
+        // Task 5: Fetch user details from a database
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+        
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+        let payload = {
+            user: {
+                id: findUser._id.toString(),
+             },
+         };
+
+        const authToken = jwt.sign(payload, JWT_SECRET);
+
+        // Task 7: Log the successful registration using the logger
+        logger.info('User sign in successfully');
+
+        // Task 8: Return the user email and the token as a JSON
+        return res.json({ authToken, userName, userEmail });
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
