8ddea8ce61cabaa89283d7a8438b7319874f43de
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-07-13 14:35:45 -0400

    new changes applied

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const pinoLogger = require('./logger');
-const path = require('path');
+const pinoLogger = require('./logger')
+const path = require('path')
 
-const connectToDatabase = require('./models/db');
+const connectToDatabase = require('./models/db')
 
-const app = express();
-app.use('*', cors());
-const port = 3060;
+const app = express()
+app.use('*', cors())
+const port = 3060
 
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
-    .catch((e) => console.error('Failed to connect to DB', e));
+    .catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json());
 
