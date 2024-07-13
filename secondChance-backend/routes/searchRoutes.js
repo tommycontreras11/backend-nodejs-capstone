78923397a4ee068ace2dc2c89bd3daa565812a0f
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-07-13 14:58:02 -0400

    new changes applied

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -4,31 +4,33 @@ const connectToDatabase = require('../models/db')
 require('dotenv').config()
 
 router.get('/', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase()
-        const collection = db.collection(process.env.MONGO_COLLECTION)
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection(process.env.MONGO_COLLECTION)
 
-        let query = {}
+    let query = {}
 
-        if (req.query.name && req.query.name.trim() !== '') {
-            query.name = { $regex: req.query.name, $options: 'i' } 
-        }
-
-        if (req.query.category) {
-            query.category = req.query.category
-        }
-        if (req.query.condition) {
-            query.condition = req.query.condition
-        }
-        if (req.query.age_years) {
-            query.age_years = { $lte: parseInt(req.query.age_years) }
-        }
-
-        const gifts = await collection.find(query).toArray()
-        return res.json(gifts)
-    } catch (e) {
-        next(e)
+    if (req.query.name && req.query.name.trim() !== '') {
+      query.name = { $regex: req.query.name, $options: 'i' }
     }
+
+    if (req.query.category) {
+      query.category = req.query.category
+    }
+    
+    if (req.query.condition) {
+      query.condition = req.query.condition
+    }
+    
+    if (req.query.age_years) {
+      query.age_years = { $lte: parseInt(req.query.age_years) }
+    }
+
+    const gifts = await collection.find(query).toArray()
+    return res.json(gifts)
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
