b86a0fb2acadc998b3aa5ba4f8ac3d760ceaa4f4
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-07-13 14:37:37 -0400

    new changes applied

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -12,27 +12,26 @@ const port = 3060
 
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e))
+}).catch((e) => console.error('Failed to connect to DB', e))
 
-app.use(express.json());
+app.use(express.json())
 
-const secondChanceRoutes = require('./routes/secondChanceItemsRoutes');
-const authRoutes = require('./routes/authRoutes');
-const searchRoutes = require('./routes/searchRoutes');
+const secondChanceRoutes = require('./routes/secondChanceItemsRoutes')
+const authRoutes = require('./routes/authRoutes')
+const searchRoutes = require('./routes/searchRoutes')
 const pinoHttp = require('pino-http');
-const logger = require('./logger');
+const logger = require('./logger')
 
-app.use(pinoHttp({ logger }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(pinoHttp({ logger }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/api/secondchance/items', secondChanceRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/secondchance/search', searchRoutes);
+app.use('/api/secondchance/items', secondChanceRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/secondchance/search', searchRoutes)
 
 app.use((err, _req, res, _next) => {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send('Internal Server Error')
 });
 
 app.get("/",(_req, res)=>{
@@ -40,5 +39,5 @@ app.get("/",(_req, res)=>{
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`)
 });
