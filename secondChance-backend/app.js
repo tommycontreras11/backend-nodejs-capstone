240fed4e15a3eb34d62d364d82110b350187b616
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-07-23 19:40:35 -0400

    feat: :sparkles: add loadData method on app

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const pinoLogger = require('./logger')
 const path = require('path')
 const connectToDatabase = require('./models/db')
+const {loadData} = require("./util/import-mongo/index");
 
 const app = express()
 app.use('*', cors())
